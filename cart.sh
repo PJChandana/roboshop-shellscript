54a0e644f6498d3198673de4e32b0425ba004c2c
PJChandana <PJChandana@gmail.com>
2024-02-09 19:37:58 +0530

    roboshop-shellscripts

--- cart.sh
+++ cart.sh
@@ -69,7 +69,7 @@ npm install  &>> $LOGFILE
 
 VALIDATE $? "Installing dependencies"
 
-cp /home/centos/roboshop-shellscript/cart.service/etc/systemd/system/cart.service
+cp '/home/centos/roboshop-shellscript/cart.service/etc/systemd/system/cart.service'
 
 VALIDATE $? "Copying cart service file"
 
