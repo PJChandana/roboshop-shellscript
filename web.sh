6499462ca41f2e782f3bb76ac092791329d28d05
PJChandana <PJChandana@gmail.com>
2024-02-09 23:34:43 +0530

    roboshop-shellscripts

--- web.sh
+++ web.sh
@@ -30,39 +30,40 @@ else
     echo "You are root user"
 fi # fi means reverse of if, indicating condition end
 
-dnf install nginx -y
+dnf install nginx -y &>> $LOGFILE
 
 VALIDATE $? "Installing nginx"
 
-systemctl enable nginx
+systemctl enable nginx &>> $LOGFILE
 
 VALIDATE $? "Enabling nginx"
 
-systemctl start nginx
+systemctl start nginx &>> $LOGFILE
 
 VALIDATE $? "Starting nginx"
 
-rm -rf /usr/share/nginx/html/
+rm -rf /usr/share/nginx/html/ &>> $LOGFILE
+
 
 VALIDATE $? "removed default website"
 
-curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip
+curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip &>> $LOGFILE
 
 VALIDATE $? "Downloading the web application"
 
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html &>> $LOGFILE
 
 VALIDATE $? "moving the html directory"
 
-unzip -o /tmp/web.zip
+unzip -o /tmp/web.zip &>> $LOGFILE
 
 VALIDATE $? "unzipping the file"
 
-cp /home/centos/roboshop-shellscript/roboshop.conf /etc/nginx/default.d/roboshop.conf 
+cp /home/centos/roboshop-shellscript/roboshop.conf /etc/nginx/default.d/roboshop.conf  &>> $LOGFILE
 
 VALIDATE $? "copied roboshop reverse proxy config"
 
-systemctl restart nginx 
+systemctl restart nginx &>> $LOGFILE
 
 VALIDATE $? "Restarting nginx"
 
