4d32df95253f8ef1efa45827e4351fcb45b02ac0
PJChandana <PJChandana@gmail.com>
2024-02-10 21:20:16 +0530

    roboshop-shellscripts

--- payment.sh
+++ payment.sh
@@ -32,6 +32,8 @@ fi # fi means reverse of if, indicating condition end
 
 dnf install python36 gcc python3-devel -y
 
+VALIDATE $? "Installing payment"
+
 id roboshop #if roboshop user does not exist, then it is failure
 if [ $? -ne 0 ]
 then
