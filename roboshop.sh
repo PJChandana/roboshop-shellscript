1872112576c544e41d8cb5898497b86b51819fc0
PJChandana <PJChandana@gmail.com>
2024-02-22 13:41:26 +0530

    ansible-playbook

--- roboshop.sh
+++ roboshop.sh
@@ -7,12 +7,12 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 for i in "${INSTANCE[@]}"
 do 
     echo "instance is : $i"
-    if [$i == "mongodb" ] || [$i == "mysql"] || [ $i == "shipping"]
+    if [$i == "mongodb" ] || [$i == "mysql"] || [ $i == "shipping" ]
     then
         INSTANCE_TYPE="t3.small"
     else
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCE_TYPE --security-group-ids sg-03ceb08bf2c630ec0 
+    aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCE_TYPE --security-group-ids sg-03ceb08bf2c630ec0 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
 done
