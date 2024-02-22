e6f92079d2ff324d43b64247797806426a736aa6
PJChandana <PJChandana@gmail.com>
2024-02-22 17:07:04 +0530

    roboshop shell

--- roboshop.sh
+++ roboshop.sh
@@ -3,8 +3,8 @@
 AMI=ami-0f3c7d07486cad139
 SG_ID=sg-03ceb08bf2c630ec0
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-ZONE_ID=Z09045333CD1CXO8E06XN
-DOMAIN_NAME="awsaiops.online"
+# ZONE_ID=Z09045333CD1CXO8E06XN
+# DOMAIN_NAME="awsaiops.online"
 
 for i in "${INSTANCES[@]}"
 do 
@@ -18,23 +18,23 @@ do
     IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCE_TYPE --security-group-ids sg-03ceb08bf2c630ec0 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instance[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
 
-    #create R53 record,make sure delete existing record
-    aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
-    --change-batch '
-    {
-        "Comment": "Creating a record set for cognito endpoint"
-        ,"Changes": [{
-        "Action"              : "UPSERT"
-        ,"ResourceRecordSet"  : {
-            "Name"              : "'$i'.'$DOMAIN_NAME'"
-            ,"Type"             : "A"
-            ,"TTL"              : 1
-            ,"ResourceRecords"  : [{
-                "Value"         : "'$IP_ADDRESS'"
-            }]
-        }
-        }]
-    }
-        '
+    # #create R53 record,make sure delete existing record
+    # aws route53 change-resource-record-sets \
+    # --hosted-zone-id $ZONE_ID \
+    # --change-batch '
+    # {
+    #     "Comment": "Creating a record set for cognito endpoint"
+    #     ,"Changes": [{
+    #     "Action"              : "UPSERT"
+    #     ,"ResourceRecordSet"  : {
+    #         "Name"              : "'$i'.'$DOMAIN_NAME'"
+    #         ,"Type"             : "A"
+    #         ,"TTL"              : 1
+    #         ,"ResourceRecords"  : [{
+    #             "Value"         : "'$IP_ADDRESS'"
+    #         }]
+    #     }
+    #     }]
+    # }
+    #     '
 done
