52f6b39d06789cb70baa16199560c9461c5ef49a
PJChandana <PJChandana@gmail.com>
2024-02-22 16:08:50 +0530

    roboshop shell

--- roboshop.sh
+++ roboshop.sh
@@ -23,18 +23,18 @@ do
     --hosted-zone-id $ZONE_ID \
     --change-batch '
     {
-        "Comment": "Testing creating a record set",
+        "Comment": "Creating a record set for cognito endpoint"
         ,"Changes": [{
-        "Action"              : "CREATE",
+        "Action"              : "CREATE"
         ,"ResourceRecordSet"  : {
-            "Name"              : "$i.$DOMAIN_NAME",
-            ,"Type"             : "A",
-            ,"TTL"              : 1,
+            "Name"              : "'$i'.'$DOMAIN_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
             ,"ResourceRecords"  : [{
-                "Value"         : "$IP_ADDRESS"
+                "Value"         : "'$IP_ADDRESS'"
             }]
         }
         }]
     }
-    "
+        "
 done
